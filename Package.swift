e7a4a9b308db4c2dc4bb487ad7de95898b03207b
Iordan Iordanov <iordan@morpheusly.com>
2022-11-08 23:30:37 -0500

    WIP

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(name: "WireGuardKit", targets: ["WireGuardKit"])
+        .library(name: "WireGuardKit", targets: ["WireGuardKit", "Shared"])
     ],
     dependencies: [],
     targets: [
