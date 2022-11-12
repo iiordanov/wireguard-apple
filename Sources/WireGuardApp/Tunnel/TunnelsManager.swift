89685b845abda129f115d73a43b7778702b5ab4f
iordan iordanov <iordanovv@gmail.com>
2022-11-12 16:40:41 -0500

    WIP

--- Sources/WireGuardApp/Tunnel/TunnelsManager.swift
+++ Sources/WireGuardApp/Tunnel/TunnelsManager.swift
@@ -5,6 +5,7 @@ import Foundation
 import NetworkExtension
 import os.log
 import WireGuardShared
+import WireGuardSharedModel
 import WireGuardKit
 
 protocol TunnelsManagerListDelegate: AnyObject {
