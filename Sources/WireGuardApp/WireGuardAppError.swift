acadf6f5d595b5db4833aa739aae8236b17625ba
iordan iordanov <iordanovv@gmail.com>
2022-11-13 15:09:06 -0500

    WIP

--- Sources/WireGuardApp/WireGuardAppError.swift
+++ Sources/WireGuardApp/WireGuardAppError.swift
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2018-2021 WireGuard LLC. All Rights Reserved.
 
-protocol WireGuardAppError: Error {
+public protocol WireGuardAppError: Error {
     typealias AlertText = (title: String, message: String)
 
     var alertText: AlertText { get }
