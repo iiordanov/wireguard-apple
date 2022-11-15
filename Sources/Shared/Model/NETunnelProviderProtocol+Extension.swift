97e3f80caea01806defdecc0ffd67c807a1f2056
iordan iordanov <iordanovv@gmail.com>
2022-11-14 19:09:36 -0500

    WIP

--- Sources/Shared/Model/NETunnelProviderProtocol+Extension.swift
+++ Sources/Shared/Model/NETunnelProviderProtocol+Extension.swift
@@ -4,6 +4,7 @@
 import NetworkExtension
 import WireGuardKit
 import WireGuardShared
+import WireGuardSharedLogging
 
 public enum PacketTunnelProviderError: String, Error {
     case savedProtocolConfigurationIsInvalid
