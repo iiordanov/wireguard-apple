3829d8821450979da0889793d941a01ed2a296ee
iordan iordanov <iordanovv@gmail.com>
2022-11-25 18:15:23 -0500

    WIP

--- Sources/WireGuardNetworkExtension/ErrorNotifier.swift
+++ Sources/WireGuardNetworkExtension/ErrorNotifier.swift
@@ -4,21 +4,21 @@
 import NetworkExtension
 import WireGuardSharedModel
 
-class ErrorNotifier {
+public class ErrorNotifier {
     let activationAttemptId: String?
 
-    init(activationAttemptId: String?) {
+    public init(activationAttemptId: String?) {
         self.activationAttemptId = activationAttemptId
         ErrorNotifier.removeLastErrorFile()
     }
 
-    func notify(_ error: PacketTunnelProviderError) {
+    public func notify(_ error: PacketTunnelProviderError) {
         guard let activationAttemptId = activationAttemptId, let lastErrorFilePath = FileManager.networkExtensionLastErrorFileURL?.path else { return }
         let errorMessageData = "\(activationAttemptId)\n\(error)".data(using: .utf8)
         FileManager.default.createFile(atPath: lastErrorFilePath, contents: errorMessageData, attributes: nil)
     }
 
-    static func removeLastErrorFile() {
+    public static func removeLastErrorFile() {
         if let lastErrorFileURL = FileManager.networkExtensionLastErrorFileURL {
             _ = FileManager.deleteFile(at: lastErrorFileURL)
         }
